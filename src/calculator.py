64908ecd9e43081633e1961d7a7ee301d15c5d4d
Joe Shull <joseph.shull@gmail.com>
2019-01-10 17:46:03 -0700

    first src templates

---
+++ src/calculator.py
@@ -0,0 +1,16 @@
+import pandas as pd 
+import numpy as np 
+
+
+class CostOfLivingCalculator():
+	#An object that takes the clean dataframe or dataprepper and performs cost-of-living calculations
+	def __init__(self):
+		pass
+
+	def calc(self):
+	#Takes income and other base *args and returns a dict with cost-of-living values
+		pass
+
+if __name__ == '__main__':
+	calculator = CostOfLivingCalculator()
+	calculator.calc()
---
+++ src/dataloader.py
@@ -0,0 +1,17 @@
+
+import pandas as pd 
+import numpy as np 
+
+class DataPrepper():
+	# An object wrapping functions that takes our raw data and transforms into a single dataframe for querying
+	def __init__(self):
+		pass
+
+	def load(self):
+	# A function that loads the raw data
+		pass
+
+
+if __name__ == '__main__':
+	prep = DataPrepper()
+	prep.load()
