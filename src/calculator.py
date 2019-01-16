34c66178528098cb373ed4aaf7da8dfcf255a7a0
Joe Shull <joseph.shull@gmail.com>
2019-01-16 10:10:19 -0700

    calculator

--- src/calculator.py
+++ src/calculator.py
@@ -1,16 +1,98 @@
 import pandas as pd 
 import numpy as np 
+from dataloader import COLDataLoader
+
+class COLCalculator():
+    #Cost of Living Calculator
+    #Instantiate with the path to the Zillow city csvs and Consumer Expenditure xlsxs
+    def __init__(self, zillow_path, ce_path):
+        self.data = COLDataLoader(zillow_path, ce_path)
+        data.load()
+        
+    def calculate(self, gross_income, city, state, married=False):
+        #Calculation function: returns a dict (or json) with the following values
+        #income_taxes (Tuple): (Float: Approx amount of taxes paid, Float: Approx. Effective Tax Rate)
+        #housing_rent (Tuple): (Int: sqft affordable at the housing % ratio, Float: Ratio of housing/income)
+        #housing_own (Tuple): (Int: sqft affordable at the housing % ratio, Float: Ratio of housing/income)
+        tax_amount, tax_rate, net_income = self._get_taxes(gross_income, city, state, married)
+        housing_rent, housing_own = self._get_housing(gross_income, city, state)
+    
+    # Tax Calculation  - Federal and State
+    def _get_taxes(self, gross_income, city, state, married):
+        #returns tax amount, tax %, and net income
+        fed_tax, fed_rate = self._calc_fed_tax(gross_income, married)
+        state_tax, state_rate = self._calc_state_tax(gross_income, state, married)
+
+        total_tax = fed_tax + fed_rate
+        net_income = gross_income - total_tax
+        effective_tax = total_tax/gross_income
+
+        return total_tax, effective_tax, net_income
 
 
-class CostOfLivingCalculator():
-	#An object that takes the clean dataframe or dataprepper and performs cost-of-living calculations
-	def __init__(self):
-		pass
+    
+    def _calc_fed_tax(self, gross_income, married):
+        #tax withholding from https://www.irs.gov/pub/irs-pdf/n1036.pdf
+        single_rate = np.array([[3800, 0, .1],
+                                [13500, 970,  .12],
+                                [43275, 4543,  .22],
+                                [88000, 14382, .24],
+                                [164525, 32748,  .32],
+                                [207900, 46628, .35],
+                                [514100, 153799, .37]]
+                                )
 
-	def calc(self):
-	#Takes income and other base *args and returns a dict with cost-of-living values
-		pass
+        single_rate = np.hstack((single_rate, 
+                                np.append(single_rate[1:,0],np.inf).reshape(-1,1)
+                                ))
+
+        married_rate = np.array([[11800, 0, .1],
+                                 [31200, 1940, .12],
+                                 [90750, 9086, .22],
+                                 [180200, 28765, .24],
+                                 [333250, 65497, .32],
+                                 [420000, 93257, .35],
+                                 [624150, 164710, .37]
+                                ])
+
+        married_rate = np.hstack((married_rate, 
+                                np.append(married_rate[1:,0],np.inf).reshape(-1,1)
+                                ))
+        rate = single_rate
+        if married:
+            rate = married_rate
+
+        mask = np.logical_and(rate[:,0]<=gross_income, rate[:,-1]>gross_income)
+
+        if mask.any():
+            threshold, base_tax, rate = rate[mask][0][0], rate[mask][0][1], rate[mask][0][2]
+            return ((base_tax + (gross_income-threshold)*rate), rate)
+        else:
+            return 0
+        
+    def _calc_state_tax(self, gross_income, state, married):
+        #Tax rates from https://github.com/TaxFoundation/facts-and-figures
+
+
+        pass 
+    
+    def _get_housing(self, gross_income, city):
+        #Returns two tuples
+        #housing_rent (Tuple): (Int: sqft affordable at the housing % ratio, Float: Ratio of housing/income)
+        #housing_own (Tuple): (Int: sqft affordable at the housing % ratio, Float: Ratio of housing/income)
+        pass
+    
+    def _get_transporation(self, gross_income, city):
+        #Returns amount of transportation ?
+        pass
+    
+    def _get_lifestyle(self, gross_income, city):
+        pass
+    
+    def _get_savings(self, gross_income, city):
+        pass
+        
 
 if __name__ == '__main__':
-	calculator = CostOfLivingCalculator()
-	calculator.calc()
+    calculator = COLCalculator(base_zil_path, base_ce_path):
+    print(calculator.calculate(100000, 'San Francisco', 'CA'))
--- src/dataloader.py
+++ src/dataloader.py
@@ -2,16 +2,61 @@
 import pandas as pd 
 import numpy as np 
 
-class DataPrepper():
-	# An object wrapping functions that takes our raw data and transforms into a single dataframe for querying
-	def __init__(self):
-		pass
 
-	def load(self):
-	# A function that loads the raw data
-		pass
+
+class COLDataLoader():
+    #A Class for loading data for the Cost of living calculator.
+    #Instantiate with the path to the Zillow city csvs and Consumer Expenditure xlsx
+    
+    def __init__(self, zillow_path, ce_path):
+        self.zillow_path = zillow_path
+        self.ce_path = ce_path
+        self.df_zil_own = None
+        self.df_zil_rent = None
+        self.df_ce = None
+        
+    def load(self):
+        #Load Zillow
+        self.df_zil_own = pd.read_csv(os.path.join(self.zillow_path, 'City_MedianValuePerSqft_AllHomes.csv'), header=0)
+        self.df_zil_rent = pd.read_csv(os.path.join(self.zillow_path,'City_MedianRentalPricePerSqft_AllHomes.csv'))
+        
+        #Load CE
+        file_list = self.get_file_list(self.ce_path)
+        list_df = self.get_dataframes(file_list)
+        self.df_ce = self.merge_dataframes(list_df)
+        
+    def get_file_list(self, base_path):
+        #Input: String - folder path to xlsx files
+        #Output: List of Strings - paths to the files
+        files = os.listdir(base_path)
+        file_list = []
+        for file in files:
+            if not file.startswith('~'):
+                file_path = os.path.join(base_path, file)
+                file_list.append(file_path)
+        return file_list
+
+    def get_dataframes(self, file_list):
+        #Input: List of Strings - paths to xlsx files
+        #Output: List of pd dataframes
+        list_df = []
+        for file in file_list:
+            df = pd.read_excel(file, header=2)
+            df = df.loc[~df.iloc[:,0].isna(),:]
+            df.set_index('Item', inplace=True)
+            list_df.append(df)
+        return list_df
+
+    def merge_dataframes(self, list_df):
+        #Input: list of pd dataframes
+        #Output: dataframe joined columnwise by index.
+        return pd.concat(list_df, axis=1, ignore_index=False)
+
+
 
 
 if __name__ == '__main__':
-	prep = DataPrepper()
-	prep.load()
+	base_ce_path = '../data/bls_ce/msa/'
+	base_zil_path = '../data/zillow/city/'
+	data = COLDataLoader(base_zil_path, base_ce_path)
+	data.load()
